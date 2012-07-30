83c7a3f391f57d6d9dce750369448a67dc265d7d
Asif Ashraf <asif.log@gmail.com>
2012-07-30 16:27:28 +0500

    Added environment helpers

---
+++ Areas.Lib.Tests/AppEnvironment/AppDomainHelperFeatures.feature
@@ -0,0 +1,8 @@
+﻿Feature: Search all assemblies in app domain
+	I want to search types in the whole app domain 
+	for different critareas
+
+Scenario: Find by attribute type
+	Given I am running any application
+	When I provide an attribute type to find all matching types
+	Then the result should be list of all classes which has the that attribute applied
---
+++ Areas.Lib/AppEnvironment/AppDomainHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Areas.Lib.AppEnvironment
+{
+    public class AppDomainHelper
+    {
+        /// <summary>
+        /// Find all types by give attribute type
+        /// </summary>
+        /// <typeparam name="TAttribute">Attribute type</typeparam>
+        /// <returns>Returns all types where the specificied attribute was found</returns>
+        public List<Type> FindTypesByAttrib<TAttribute>()
+        {
+            return (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                         from type in assembly.GetTypes()
+                    where Attribute.IsDefined(typeof(TAttribute), type)
+                         select type).ToList();
+
+        }
+    }
+}
+
