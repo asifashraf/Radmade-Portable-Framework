1943378568c5fb95925b9b08f87b31a1f2ab9106
Asif Ashraf <asif.log@gmail.com>
2012-07-31 16:43:01 +0500

    renamed dotnet extensions
    added clean solution

--- Areas.DotNetExtensions/Properties/AssemblyInfo.cs
+++ Areas.DotNetExtensions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Areas.DotNetExtentions")]
+[assembly: AssemblyTitle("Areas.DotNetExtensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Areas.DotNetExtentions")]
+[assembly: AssemblyProduct("Areas.DotNetExtensions")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b2df0e3-7573-461b-a5e0-b1be0d3d8b89")]
+[assembly: Guid("57ede9d1-ca2b-4f0b-8017-95ba89829343")]
 
 // Version information for an assembly consists of the following four values:
 //
