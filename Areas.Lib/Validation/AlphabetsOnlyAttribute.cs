7678355458012cf7c400d671d301a51b7a5f5a6a
Asif Ashraf <asif.log@gmail.com>
2012-11-27 03:52:40 +0500

    Added Validation folder for custom validations

---
+++ Areas.Lib/Validation/AlphabetsOnlyAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebAreas.Lib.Validation
+{
+    public class AlphabetsOnlyAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var regex = new Regex(@"^[a-zA-Z]+$");
+            if (regex.IsMatch(value.Text()).Not())
+            {
+                return new ValidationResult("Only alphabets allowed");
+            }
+            return ValidationResult.Success;
+        }
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            ModelClientValidationRule rule = new ModelClientValidationRule();
+            rule.ValidationType = "alphabetsOnly";
+            rule.ErrorMessage = "Only alphabets allowed";
+            //rule.ValidationParameters.Add
+            //("param", DateTime.Now.ToString("dd-MM-yyyy"));
+            return new List<ModelClientValidationRule> 
+            { 
+                rule 
+            };
+        }
+    }
+}
---
+++ Areas.Lib/Validation/MaxCharAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace WebAreas.Lib.Validation
+{
+    public class MaxCharAttribute : MaxLengthAttribute
+    {
+        public MaxCharAttribute(int length)
+            : base(length)
+        {
+
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format("Maximum {0} characters are allowed for {1}", this.Length, name);
+        }
+    }
+}
