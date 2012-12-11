972e38f958ad601117126f19c0ce0f88e9c50d40
Asif Ashraf <asif.log@gmail.com>
2012-12-11 15:15:26 +0500

    Serialize Validation Error

---
+++ Areas.DotNetExtensions/System.Web.MVC/ModelStateDictionaryX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using WebAreas.DotNetExtensions.System.Web.MVC;
+
+public static class ModelStateDictionaryX
+{
+    public static List<FieldErrorsDetail> Errors(this ModelStateDictionary modelState)
+    {
+        var errors = new List<FieldErrorsDetail>();
+        foreach (var key in modelState.Keys)
+        {
+            var error = new FieldErrorsDetail();
+            error.Name = key;
+            error.Errors = modelState[key].Errors.Select(e => e.ErrorMessage).ToListSafely();
+            error.Value = modelState[key].Value.RawValue;
+            errors.Add(error);
+        }
+        return errors;
+    }
+}
+
